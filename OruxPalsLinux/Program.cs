df6a80cd6fad7bed25a8eef0ae335e7ff54abbd6
dkxce <25371256+dkxce@users.noreply.github.com>
2023-06-13 18:23:35 +0300

    Add files via upload
    .Net 7.0 Version (Linux, Ubuntu) Release

---
+++ OruxPalsLinux/Program.cs
@@ -0,0 +1,25 @@
+﻿using OruxPals;
+using System.Collections;
+using System.ComponentModel;
+using System.Text.RegularExpressions;
+using System.Xml;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+        OruxPals.OruxPalsServer ops = new OruxPals.OruxPalsServer();
+        ops.Start();
+        while (true)
+        {
+            Console.WriteLine("enter: /exit to exit");
+            Console.WriteLine("enter: /restart to restart");
+            string? line = Console.ReadLine();
+            if (line == "/exit") break;
+            if (line == "/restart") { ops.Stop(); ops = new OruxPals.OruxPalsServer(); ops.Start();  };
+        };
+        ops.Stop();
+        return;
+    }
+}
